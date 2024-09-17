97811df28f7192c083b1df4ca28e6655968f0bf8
Mohammad Mohammadzade <mohammad1382217@gmail.com>
2024-09-17 14:00:06 +0330

    add button and loginForm component

--- app/componects/input.tsx
+++ app/componects/input.tsx
@@ -1,6 +1,11 @@
 'use client'
 
-import { Input } from "@nextui-org/input";
+import dynamic from 'next/dynamic';
+import Skeleton from 'react-loading-skeleton';
+
+const Input = dynamic(() => import("@nextui-org/input").then((mod) => mod.Input), {
+    loading: () => <Skeleton height={40} width={256} />,
+});
 
 interface ClassNamesType {
     label?: string;
@@ -8,31 +13,38 @@ interface ClassNamesType {
     inputWrapper?: string;
     innerWrapper?: string;
 }
+
 interface InputType {
     name?: string;
     label?: string;
-    type?: string,
+    type?: string;
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     variant?: "flat" | "faded" | "bordered" | "underlined" | undefined;
-    classNames?: ClassNamesType; // Use the ClassNamesType here
+    classNames?: ClassNamesType;
 }
+
 const InputZiva: React.FC<InputType> = ({ value, onChange, name, label, variant, type, classNames }) => {
     return (
-        <Input value={value}
+        <Input 
+            id={name}
+            value={value}
             onChange={onChange}
             name={name}
             variant={variant}
             dir="rtl"
             lang="fa"
             type={type}
+            aria-label={label}
             label={label}
             classNames={{
                 label: `!self-end mr-0 !origin-top-right ${classNames?.label}`,
                 input: `text-right mr-1 ${classNames?.input}`,
                 inputWrapper: `w-64 ${classNames?.inputWrapper}`,
                 innerWrapper: `${classNames?.innerWrapper}`
-            }} />
-    )
+            }} 
+        />
+    );
 }
-export { InputZiva };
+
+export default InputZiva;
