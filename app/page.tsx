66c454530a2bc88f391a0eae080d96661f020891
Mohammad Mohammadzade <mohammad1382217@gmail.com>
2024-09-17 23:44:46 +0330

    full login responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import LoginForm from "./componects/LoginForm";
-import bg_login from "./assests/RectangleLoginPage.png"
+import bg_login from "./assests/RectangleLoginPage.webp"
 import Image from "next/image";
 
 const Home: React.FC = () => {
   return (
     <main
       lang="fa-IR"
-      className="absolute flex text-black w-full min-h-screen flex-col items-center p-6"
+      className="absolute flex text-black w-full min-h-screen flex-col items-center sm:p-6"
     >
       <Image
-        className="relative top-0 w-full h-[280px] flex flex-col self-start" src={bg_login} alt={"bg_login"} width={1392} height={280}
+        className="relative top-0 w-full h-[280px] flex flex-col self-start rounded-none sm:rounded-xl" src={bg_login} alt={"bg_login"} width={1392} height={280}
       />
       <LoginForm />
     </main>
